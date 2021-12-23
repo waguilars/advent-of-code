a6036c234850fd15c267bbeed771fcacda932b2c
Wilson Aguilar <52500170+waguilars@users.noreply.github.com>
2021-12-22 19:51:04 -0500

    :sparkles: submitted day01 solution

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "input";
+
+    let content = fs::read_to_string(filename).expect("Cannot read file");
+
+    let measurements: Vec<i32> = content
+        .split('\n')
+        .filter(|val| !val.is_empty())
+        .map(|str| str.parse().expect("Cannot parse value"))
+        .collect();
+
+    let mut increased_times = 0;
+
+    for i in 1..measurements.len() {
+        if measurements[i] > measurements[i - 1] {
+            increased_times += 1;
+        }
+    }
+
+    println!("increased times --> {}", increased_times);
 }
